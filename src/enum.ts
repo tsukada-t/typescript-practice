0fef00dbd21f01fa1022ca6a360d03bdc14c21a8
tsukada-t <t.tsukada1122@gmail.com>
2021-08-27 22:20:04 +0900

    new type

---
+++ src/enum.ts
@@ -0,0 +1,36 @@
+export {};
+
+enum Months {
+  January = 1,
+  February,
+  March,
+  April,
+  May,
+  Jun,
+  July,
+  August,
+  September,
+  October,
+  November,
+  December,
+}
+
+console.log(Months.July);
+console.log(Months.January);
+
+enum COLORS {
+  RED = '#FF0000',
+  WHITE = '#FFFFF',
+  GREEN = '#008000',
+  BLUE = '#0000FF',
+  BLACK = '000000'
+}
+
+console.log(COLORS.RED);
+
+enum COLORS {
+  YELLOW = '#FFFF00',
+  GRAY = '#808080'
+}
+
+console.log(COLORS.YELLOW)
